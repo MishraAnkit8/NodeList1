435f477dcaf3502dfdead23d560b0d67ba78153d
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-10-30 17:29:18 +0530

    Update models.js

--- NodeDb/src/models/models.js
+++ NodeDb/src/models/models.js
@@ -4,18 +4,24 @@ module.exports.fetchAll = ()=>{
     return pgPool.query(`SELECT id,name FROM userData3`);
 }
 
-module.exports.create =(taskName) =>{
+
+module.exports.create =(taskId) =>{
     const sql = {
-        text: 'INSERT INTO dataUser(name) VALUES($1)',
-        values: [taskName]
+        text: `INSERT INTO userdata3(name) VALUES($1) RETURNING id  `,
+        values: [taskId] 
+        
+        
     }
+    console.log('sql object >>>>>>>>>>>>>>>>>',sql.values);
+    console.log('>>>>>>>>>>>>>>>>>>',sql.text)
+   
     return pgPool.query(sql);
 }
 
-module.exports.delete = (taskName) => {
+module.exports.delete = (taskId) => {
     return new Promise((resolve, reject) => {
 
-        pgPool.query('DELETE FROM dataUser WHERE name = $1', [taskName], (err, result) => {
+        pgPool.query('DELETE FROM userdata3 WHERE id = $1', [taskId], (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -25,3 +31,13 @@ module.exports.delete = (taskName) => {
     });
 };
 
+module.exports.update = (taskId, taskName) => {
+    const sql = {
+        text: 'UPDATE userdata3 SET name = $1 WHERE id = $2',
+        values: [taskName, taskId]
+    };
+    console.log('sql object >>>>>>>>>>>>>>>>>', sql.values);
+    console.log('>>>>>>>>>>>>>>>>>>', sql.text);
+
+    return pgPool.query(sql);
+}
