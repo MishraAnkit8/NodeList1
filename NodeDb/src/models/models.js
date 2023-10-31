a76f10590327cc2f3ed2120aaf11b9350c243d93
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-10-31 18:09:04 +0530

    Update models.js

--- NodeDb/src/models/models.js
+++ NodeDb/src/models/models.js
@@ -1,27 +1,26 @@
 const {pgPool}  = require('../../config/db.config');
 
 module.exports.fetchAll = ()=>{
-    return pgPool.query(`SELECT id,name FROM userData3`);
+    return pgPool.query(`SELECT id,name FROM dataList`);
 }
 
 
-module.exports.create =(taskId) =>{
+module.exports.create = (taskId) => {
     const sql = {
-        text: `INSERT INTO userdata3(name) VALUES($1) RETURNING id  `,
-        values: [taskId] 
-        
-        
-    }
-    console.log('sql object >>>>>>>>>>>>>>>>>',sql.values);
-    console.log('>>>>>>>>>>>>>>>>>>',sql.text)
-   
+        text: 'INSERT INTO dataList(name) VALUES($1) RETURNING id',
+        values: [taskId]
+    };
+
+    console.log('SQL object:', sql.text);
+    console.log('Task ID:', sql.values[0]);
+
     return pgPool.query(sql);
-}
+};
 
 module.exports.delete = (taskId) => {
     return new Promise((resolve, reject) => {
 
-        pgPool.query('DELETE FROM userdata3 WHERE id = $1', [taskId], (err, result) => {
+        pgPool.query('DELETE FROM dataList WHERE id = $1', [taskId], (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -33,7 +32,7 @@ module.exports.delete = (taskId) => {
 
 module.exports.update = (taskId, taskName) => {
     const sql = {
-        text: 'UPDATE userdata3 SET name = $1 WHERE id = $2',
+        text: 'UPDATE dataList SET name = $1 WHERE id = $2',
         values: [taskName, taskId]
     };
     console.log('sql object >>>>>>>>>>>>>>>>>', sql.values);
